71ee5781c3c1a590bdad94e41e7712ba14638a4b
unchangingConstant <ebegleyc@gmail.com>
2025-11-17 13:37:01 -0500

    Last changes before I delete from pc

--- src/main/java/io/github/unchangingconstant/studenttracker/app/controllers/WindowController.java
+++ src/main/java/io/github/unchangingconstant/studenttracker/app/controllers/WindowController.java
@@ -2,7 +2,10 @@ package io.github.unchangingconstant.studenttracker.app.controllers;
 
 import io.github.unchangingconstant.studenttracker.app.Controller;
 
-public class MainController implements Controller {
+public class WindowController implements Controller {
+
+    public WindowController()   {
+    }
 
     @Override
     public void initialize() {
